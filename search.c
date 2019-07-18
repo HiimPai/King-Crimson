f8cb7bd45dbf3349487a818337048d656b4eb02f
HiimPai <45031857+HiimPai@users.noreply.github.com>
2019-07-18 14:33:10 +0200

    Add files via upload

---
+++ search.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <collectc/hashtable.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <string.h>
+
+HashTable *table;
+int size = 3;
+struct stat arrayFileStat[3];
+
+
+// Metodo per la scrittura del file di output
+void writeOut (char *pathname) {
+ FILE* fout = fopen("/home/luca/Desktop/filestat.txt","a+");
+ struct stat fileStat;
+ stat(pathname, &fileStat);
+ array_boi(fileStat);
+ hashtable_add(table, pathname, &arrayFileStat);
+ }
+
+void array_boi(struct stat stando) {
+ int index = 0;
+ arrayFileStat[index] = stando;
+ index++;
+}
+    // Scrittura dei parametri richiesti
+   
+
+void createTable() {
+ enum cc_stat hsh = hashtable_new(&table);
+ if (hsh == CC_ERR_ALLOC) {
+  printf("polnareff");
+ }
+} 
+
+ void printTable() {
+ HashTableIter hti;
+ hashtable_iter_init(&hti, table);
+ TableEntry *entry;
+ FILE* fout = fopen("/home/luca/Desktop/filestat.txt","a+");
+ while(hashtable_iter_next(&hti, &entry) != CC_ITER_END) { 
+  fprintf(fout,"# <%s>\n", ((const char*)entry -> key));
+  fprintf(fout,"<%d> ",(((struct stat*) (entry) -> value ) -> st_uid));
+  fprintf(fout,"<%d> ",(((struct stat*) entry -> value ) -> st_gid));
+  fprintf(fout,"<%ld> ",(((struct stat*) entry -> value )-> st_size));
+  fprintf(fout,(S_ISDIR(((struct stat*)(entry -> value)) -> st_mode)) ? "<d" : "<-");
+  fprintf(fout,(((struct stat*)(entry -> value)) -> st_mode & S_IRUSR) ? "r" : "-");
+  fprintf(fout,(((struct stat*)(entry -> value)) -> st_mode & S_IWUSR) ? "w" : "-");
+  fprintf(fout,(((struct stat*)(entry -> value)) -> st_mode & S_IXUSR) ? "x" : "-");
+  fprintf(fout,(((struct stat*)(entry -> value)) -> st_mode & S_IRGRP) ? "r" : "-");
+  fprintf(fout,(((struct stat*)(entry -> value)) -> st_mode & S_IWGRP) ? "w" : "-");
+  fprintf(fout,(((struct stat*)(entry -> value)) -> st_mode & S_IXGRP) ? "x" : "-");
+  fprintf(fout,(((struct stat*)(entry -> value)) -> st_mode & S_IROTH) ? "r" : "-");
+  fprintf(fout,(((struct stat*)(entry -> value)) -> st_mode & S_IWOTH) ? "w" : "-");
+  fprintf(fout,(((struct stat*)(entry -> value)) -> st_mode & S_IXOTH) ? "x>" : "->");
+  fprintf(fout,"###");
+ }
+} 
+
+
+
+int main (void){
+ createTable();
+ writeOut("/home/luca/Desktop/eieh.c");
+ writeOut("/home/luca/Desktop/testfilestat");
+ printTable();
+}
